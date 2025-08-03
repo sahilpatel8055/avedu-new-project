1e28ec5d3383787f23370c9a23f6392db79741f1
sahilpatel8055 <Sahilpatelbs@gmail.com>
2025-08-04 00:15:01 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useLocation } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { useEffect } from "react";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
@@ -11,20 +10,23 @@ import IGNOU from "./pages/university/IGNOU";
 import Manipal from "./pages/university/Manipal";
 import Uttaranchal from "./pages/university/Uttaranchal";
 import VGU from "./pages/university/VGU";
+// Old course pages (from your home page) - DO NOT REMOVE
 import OnlineMBA from "./pages/courses/OnlineMBA";
 import OnlineTech from "./pages/courses/OnlineTech";
 import OnlineBBA from "./pages/courses/OnlineBBA";
 import OnlineCommerce from "./pages/courses/OnlineCommerce";
 import OnlineArts from "./pages/courses/OnlineArts";
+// NEW dynamic course page
+import CoursePage from "./pages/course/CoursePage";
 
 // Component to scroll to top on route change
 const ScrollToTop = () => {
   const location = useLocation();
-  
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [location.pathname]);
-  
+
   return null;
 };
 
@@ -43,11 +45,14 @@ const App = () => (
           <Route path="/university/manipal" element={<Manipal />} />
           <Route path="/university/uttaranchal" element={<Uttaranchal />} />
           <Route path="/university/vgu" element={<VGU />} />
+          {/* Your original course routes - these remain unchanged */}
           <Route path="/courses/mba" element={<OnlineMBA />} />
           <Route path="/courses/engineering" element={<OnlineTech />} />
           <Route path="/courses/bba" element={<OnlineBBA />} />
           <Route path="/courses/commerce" element={<OnlineCommerce />} />
           <Route path="/courses/arts" element={<OnlineArts />} />
+          {/* The new dynamic route for the IGNOU courses */}
+          <Route path="/course/:courseId" element={<CoursePage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
