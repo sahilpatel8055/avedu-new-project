952aa233d764d0b8d839d2bc156cc153afa87f28
sahilpatel8055 <Sahilpatelbs@gmail.com>
2025-08-08 12:08:42 +0530

    Update career-assistance.tsx

--- src/components/ui/career-assistance.tsx
+++ src/components/ui/career-assistance.tsx
@@ -17,7 +17,7 @@ const CareerAssistance = () => {
         </p>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 items-center">
           {/* Left side with image */}
-          <div className="relative overflow-hidden rounded-lg shadow-lg h-[42.5rem]">
+          <div className="relative overflow-hidden rounded-lg shadow-lg h-[50.5rem]">
             <img 
               src={placementImage} 
               alt="A working professional" 
